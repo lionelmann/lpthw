62867496a65c3411125792c57db0dc2dafb9bb31
Lionel Mann <lionelmann@gmail.com>
2017-02-27 15:09:34 -0500

    exercises 7 - 10

---
+++ ex9.py
@@ -0,0 +1,9 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+
+print "Here are the days", days
+print "Here are the months", months
+
+print """
+A comment on
+multiple lines"""
